f41446188c73d643b88efdd8c99b13c92f6eb6fc
Barak Korren <barak.korren@gmail.com>
2021-05-10 21:11:40 +0300

    Not working attempt to use OpenGLContext
    There are parts in OpenGLContext that are incompatible with Python 3.

--- main.py
+++ main.py
@@ -2,107 +2,72 @@
 An implementation of the OpenglContext tutorials without OpenglContext
 http://pyopengl.sourceforge.net/context/tutorials/index.html
 """
-from functools import lru_cache
-import pygame as pg
+from OpenGLContext import testingcontext
+
 from OpenGL.GL import *
 from OpenGL.arrays import vbo
-from numpy import array
+from OpenGLContext.arrays import array
 from OpenGL.GL import shaders
 
 
-@lru_cache(1)
-def vertex_shader():
-    return shaders.compileShader(
-        """#version 120
-        void main() {
-            // This is not really working because OpenGLContext did not set the projection matrix up for us 
-            gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-        }""",
-        GL_VERTEX_SHADER
-    )
+BaseContext = testingcontext.getInteractive("pygame")
 
 
-@lru_cache(1)
-def fragment_shader():
-    return shaders.compileShader(
-        """#version 120
-        void main() {
-            gl_FragColor = vec4( 0, 1, 0, 1 );
-        }""",
-        GL_FRAGMENT_SHADER
-    )
+class TestContext(BaseContext):
+    """Create a simple vertex shader"""
+
+    def OnInit(self):
+        vertex_shader = shaders.compileShader(
+            """#version 120
+            void main() {
+                gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+            }""",
+            GL_VERTEX_SHADER
+        )
+        fragment_shader = shaders.compileShader(
+            """#version 120
+            void main() {
+                gl_FragColor = vec4( 0, 1, 0, 1 );
+            }""",
+            GL_FRAGMENT_SHADER
+        )
+        # noinspection PyAttributeOutsideInit
+        self.shader = shaders.compileProgram(vertex_shader, fragment_shader)
+
+        # noinspection PyAttributeOutsideInit
+        self.vbo = vbo.VBO(
+            array([
+                [0, 1, 0],
+                [-1, -1, 0],
+                [1, -1, 0],
+                [2, -1, 0],
+                [4, -1, 0],
+                [4, 1, 0],
+                [2, -1, 0],
+                [4, 1, 0],
+                [2, 1, 0],
+            ], 'f')
+        )
 
 
-@lru_cache(1)
-def shader():
-    return shaders.compileProgram(vertex_shader(), fragment_shader())
-
-
-@lru_cache()
-def buildVbo():
-    return vbo.VBO(
-        array([
-            [0, 1, 0],
-            [-1, -1, 0],
-            [1, -1, 0],
-            [2, -1, 0],
-            [4, -1, 0],
-            [4, 1, 0],
-            [2, -1, 0],
-            [4, 1, 0],
-            [2, 1, 0],
-        ], 'f')
-    )
-
-
-def render():
-    shaders.glUseProgram(shader())
-    vbo = buildVbo()
-    try:
-        vbo.bind()
+    def Render(self):
+        shaders.glUseProgram(self.shader)
         try:
-            glEnableClientState(GL_VERTEX_ARRAY)
-            glVertexPointerf(vbo)
-            glDrawArrays(GL_TRIANGLES, 0, 9)
+            self.vbo.bind()
+            try:
+                glEnableClientState(GL_VERTEX_ARRAY)
+                glVertexPointerf(vbo)
+                glDrawArrays(GL_TRIANGLES, 0, 9)
+            finally:
+                self.vbo.unbind()
+                glDisableClientState(GL_VERTEX_ARRAY)
+
         finally:
-            vbo.unbind()
-            glDisableClientState(GL_VERTEX_ARRAY)
-
-    finally:
-        shaders.glUseProgram(0)
-
-
-def screen_init():
-    pg.init()
-    # Initialize a new version of OpenGL that can do shaders
-    # https://github.com/pygame/pygame/issues/1124
-    # (It seems to not really be needed as it works on my T440s without this, and I needed to play with the version
-    # numbers a bit to make it work because the version from the URL above did not work for me)
-    pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
-    pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 0)
-    pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
-
-    display = (1600, 800)
-    pg.display.set_mode(display, pg.DOUBLEBUF | pg.OPENGL)
-
-
-def main_lopp():
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                quit()
-
-        # glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        render()
-
-        pg.display.flip()
-        pg.time.wait(10)
+            shaders.glUseProgram(0)
 
 
 def main():
-    screen_init()
-    main_lopp()
+    TestContext.ContextMainLoop()
 
 
 if __name__ == "__main__":
