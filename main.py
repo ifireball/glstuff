15ab747a545a9b48453177528cf85fe71f2c0224
Barak Korren <barak.korren@gmail.com>
2021-05-17 20:20:44 +0300

    Modern OpenGL tutorial 2.2 (Pyglet)
    Python version of the tutorial at:
    https://paroj.github.io/gltut/index.html
    
    Ported the implementation to use Pyglet,
    pyshaders and pyglbuffers instead of PyOpenGL

--- main.py
+++ main.py
@@ -1,75 +1,65 @@
-"""
-An implementation of the OpenglContext tutorials without OpenglContext
-http://pyopengl.sourceforge.net/context/tutorials/index.html
-"""
-import pygame as pg
-from OpenGL.GL import *
-from OpenGL.arrays import vbo
-from numpy import array
-from OpenGL.GL import shaders
+from ctypes import byref, c_uint
 
-from glwrappers import gl_enabled_vertex_attrib_array, boundVBO, shader
+import pyglet
+from pyglet.gl import *
+import pyshaders
+from pyglbuffers import Buffer
 
 
-def vertex_shader() -> GLuint:
-    return shaders.compileShader(
-        """#version 330
-
-        layout(location = 0) in vec4 position;
-        layout(location = 1) in vec4 color;
-
-        smooth out vec4 theColor;
-
-        void main()
-        {
-           gl_Position = position;
-           theColor = color;
-        }""",
-        GL_VERTEX_SHADER
-    )
+pyshaders.load_extension('pyglbuffers_bindings')
 
 
-def fragment_shader() -> GLuint:
-    return shaders.compileShader(
-        """#version 330
-
-        smooth in vec4 theColor;
-        
-        out vec4 outputColor;
-        
-        void main()
-        {
-            outputColor = theColor;
-        }""",
-        GL_FRAGMENT_SHADER
-    )
-
-
-theProgram: shaders.ShaderProgram = None
+theProgram: pyshaders.ShaderProgram
 
 
 def initialize_program():
     global theProgram
-    theProgram = shaders.compileProgram(vertex_shader(), fragment_shader())
+
+    vertex_shader = \
+        """#version 330 core
+
+        in vec4 position;
+        in vec4 color;
+
+        smooth out vec4 vertex_color;
+
+        void main()
+        {
+           gl_Position = position;
+           vertex_color = color;
+        }
+        """
+
+    fragment_shader = \
+        """#version 330 core
+
+        smooth in vec4 vertex_color;
+
+        out vec4 final_color;
+
+        void main()
+        {
+            final_color = vertex_color;
+        }
+        """
+
+    theProgram = pyshaders.from_string(vertex_shader, fragment_shader)
+    print(theProgram)
+    print(theProgram.attributes)
 
 
-positionBufferObject: vbo.VBO = None
-vao: GLuint
+positionBufferObject: Buffer = None
+vao: GLuint = c_uint(0)
 
 
 def initialize_vertex_buffer():
     global positionBufferObject
-    positionBufferObject = vbo.VBO(
-        array([
-            0.0,     0.5, 0.0, 1.0,
-            0.5,  -0.366, 0.0, 1.0,
-            -0.5, -0.366, 0.0, 1.0,
-            1.0, 0.0, 0.0, 1.0,
-            0.0, 1.0, 0.0, 1.0,
-            0.0, 0.0, 1.0, 1.0,
-        ], 'f'),
-        usage=GL_STATIC_DRAW
-    )
+    positionBufferObject = Buffer.array('(4f)[position] (4f)[color]', GL_STATIC_DRAW)
+    positionBufferObject.init((
+        ((0.0,     0.5, 0.0, 1.0), (1.0, 0.0, 0.0, 1.0)),
+        ((0.5,  -0.366, 0.0, 1.0), (0.0, 1.0, 0.0, 1.0)),
+        ((-0.5, -0.366, 0.0, 1.0), (0.0, 0.0, 1.0, 1.0)),
+    ))
 
 
 def init():
@@ -77,55 +67,39 @@ def init():
     initialize_vertex_buffer()
 
     global vao
-    vao = glGenVertexArrays(1)
+    glGenVertexArrays(1, byref(vao))
     glBindVertexArray(vao)
 
 
 def display():
-    glClearColor(0, 0, 0, 0)
+    global theProgram
+
+    glClearColor(0.0, 0.0, 0.0, 1.0)
     glClear(GL_COLOR_BUFFER_BIT)
 
-    with shader(theProgram):
-        with boundVBO(positionBufferObject):
-            with gl_enabled_vertex_attrib_array(0):
-                with gl_enabled_vertex_attrib_array(1):
-                    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, positionBufferObject)
-                    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, positionBufferObject+48)
+    positionBufferObject.bind()
 
-                    glDrawArrays(GL_TRIANGLES, 0, 3)
-
-
-def screen_init():
-    pg.init()
-    # Initialize a new version of OpenGL that can do shaders
-    # https://github.com/pygame/pygame/issues/1124
-    # (It seems to not really be needed as it works on my T440s without this, and I needed to play with the version
-    # numbers a bit to make it work because the version from the URL above did not work for me)
-    pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
-    pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 1)
-    pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
-
-    display_size = (1600, 800)
-    pg.display.set_mode(display_size, pg.DOUBLEBUF | pg.OPENGL)
+    theProgram.use()
+    try:
+        theProgram.enable_all_attributes()
+        theProgram.map_attributes(positionBufferObject)
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+    finally:
+        theProgram.clear()
 
 
-def main_loop():
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                quit()
+class MainWindow(pyglet.window.Window):
+    def __init__(self):
+        super().__init__(resizable=True)
+        init()
 
+    def on_draw(self):
         display()
-
-        pg.display.flip()
-        pg.time.wait(10)
 
 
 def main():
-    screen_init()
-    init()
-    main_loop()
+    MainWindow()
+    pyglet.app.run()
 
 
 if __name__ == "__main__":
